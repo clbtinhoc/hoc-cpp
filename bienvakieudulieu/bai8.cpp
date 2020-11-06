5c85e13a1e405a55514cd596c37a07d1dd6defe8
Duy Pham Le <phamleduy04@gmail.com>
2020-11-06 11:20:04 -0600

    update cho ân =))

---
+++ bienvakieudulieu/bai8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    double a = 10.5;
+    double b = 7;
+    cout << "a / b = " << a / b;
+
+    return 0;
+
+}   
---
+++ bienvakieudulieu/bai9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a = 12984019;
+    int b = 12948120;
+    cout << (long long)a * b;
+    
+    return 0;
+
+}   
