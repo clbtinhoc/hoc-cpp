dcaae63d670d9f585afebc07901a403c1e1f8c67
Duy Pham Le <phamleduy04@gmail.com>
2020-11-13 13:53:54 -0600

    update code

---
+++ toantudauvao/bai13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    int a, b;
+    cout << "Nhập a: ";
+    cin >> a;
+    cout << "Nhập b: ";
+    cin >> b;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "a % b = " << a % b << endl;
+	return 0;
+}
